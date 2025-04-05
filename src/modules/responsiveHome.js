e133dfcfc359e655ae832456ff3e8ae866342cc6
TaimourShmait <taimourshmaits@gmail.com>
2025-04-06 01:13:26 +0300

    Added module file for a simple responsive page

---
+++ src/modules/responsiveHome.js
@@ -0,0 +1,52 @@
+import {goContact} from "../index.js";
+import {createLowerHalf} from "./home.js";
+import {createMenu} from "./menu.js";
+import {createHistoryPage} from "./history.js";
+import {createForm} from "./jobListings.js";
+ 
+function generateResponsivePage () {
+    const responsiveContainer = document.createElement("div");
+    responsiveContainer.id = "responsive-container";
+
+    // const buttonsContainer = document.createElement("div");
+    // buttonsContainer.id = "buttons-container-home-responsive";
+
+    // const contactButton = document.createElement("button");
+    // contactButton.textContent = "Contact";
+
+    // contactButton.addEventListener("click", goContact);
+      
+    // buttonsContainer.appendChild(contactButton);
+    
+    const topContent = createLowerHalf();
+
+    responsiveContainer.appendChild(topContent);
+    // responsiveContainer.appendChild(buttonsContainer);
+
+    const menuSection = document.createElement("div");
+    menuSection.id = "menu-section-responsive";
+
+    const menuText = document.createElement("p");
+    menuText.textContent = "Menu";
+
+    const menuContent = createMenu();
+    menuContent.id = "menu-content-responsive";
+
+    menuSection.appendChild(menuText);
+    menuSection.appendChild(menuContent);
+
+    responsiveContainer.appendChild(menuSection);
+
+    const footerTextInput = document.querySelector("#footer-text-input");
+    footerTextInput.setAttribute("placeholder", "Please don't hesitate to leave any questions or remarks!");
+
+
+    return responsiveContainer;
+}   
+
+export function makeResponsive () {
+    const main = document.querySelector("main");
+    main.innerHTML = "";
+    const contentDiv = generateResponsivePage();
+    main.appendChild(contentDiv);
+}
